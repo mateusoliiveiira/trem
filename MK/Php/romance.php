ca15c93b13784e48c48545394ed99180199cbdef
kauasilval <kaua60033@gmail.com>
2024-03-22 10:35:07 -0300

    as

---
+++ MK/Php/romance.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/romance.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+        <img class="logo" src="../img/logoo.jpeg" width="80px">
+        <h1>Abc Fanfiction</h1>
+    </header>
+
+    <div class="barra">
+        <nav class="link">
+            <a href="index.php">PAGINA INICIAL</a>
+            <a href="../php/tags.php">TAGS</a>
+            <a href="../php/tags.php">NAVEGAR</a>
+            <a href="../php/tags.php">MINHAS HISTORIAS</a>
+        </nav>
+    </div>
+
+    <div class="container">
+        <div class="title">
+            <h2>Tags de Romance</h2>
+        </div>
+        <ul class="tags">
+            <li class="tag">
+                <img src="../img/romance1.png" alt="Livro 1">
+                <div class="tag-info">
+                    <h3>Namorado Quase Perfeito</h3>
+                    <p>Autor: ORUMAITOU</p>
+                    <p>Data Da Publicação: 30/07/2019</p>
+                    <button onclick="showSummary('summary1')">Mostrar Resumo</button>
+                    <p id="summary1" style="display: none;">Park Jimin levava uma vida tecnicamente feliz; tinha uma boa casa, 
+                    cursava uma boa faculdade e tinha um bom namorado. Tudo estava excepcionalmente bem, até receber a terrível notícia de que seu namorado havia se apaixonado por outro e estava terminando seu relacionamento. Frustrado o bastante para sequer querer sair de casa, é persuadido por uma boba propaganda de um “namorado perfeito”, na qual os vendedores garantiam um boneco-humano capaz de ser um excelente namorado, 
+                    e acreditando ser uma brincadeira idiota, o Park não pensa duas vezes antes de fazer sua encomenda no site.
+                  </p>
+                </div>
+            </li>
+            <li class="tag">
+                <img src="../img/romance2.jpg" alt="Livro 2">
+                <div class="tag-info">
+                    <h3>Girl Meets Evil</h3>
+                    <p>Autor: Ghost_Fanfics</p>
+                    <p>Ano Da Publicação: 13/11/2020</p>
+                    <button onclick="showSummary('summary2')">Mostrar Resumo</button>
+                    <p id="summary2" style="display: none;">Nasci em Juggi, uma cidadezinha perdida na Coreia do Sul, mas a deixei por não suportar a dor da perda da minha mãe e da minha irmã mais velha. Appa e eu nos mudamos para Seul, que me acolheu e me reergueu como um lar. Mudei de escola, fiz novos amigos, 
+                    gostava da minha nova casa. Ali, nada era lembrança de um tempo que não voltaria. Era onde eu criava novas memórias para tornar minha vida suportável.Tudo parecia bem, até agora. De repente, Appa anuncia seucasamento com a Srta. Jeon. De uma vez só preciso arrumar minhas malas, sair da escola, me despedir dos meus amigos e voltar a Juggi, um lugar que odeio. A única parte boa da história é ganhar uma madrasta tão boa, mas até mesmo ela tem seu porém. Seu filho, 
+                    que se tornará meu "irmão" em poucos dias, Jeon "Arrogante" Jungkook. Ele é um espécime detestável da raça humana. Implicante, prepotente, encrenqueiro e espaçoso. Eu não o suporto, e não quero nem respirar o mesmo ar que ele, quanto mais dividir a casa, o pai, a vida.</p>
+                </div>
+            </li>
+            <li class="tag">
+                <img src="../img/romance3.png" alt="Livro 3">
+                <div class="tag-info">
+                    <h3>Danger (Imagine Park Jimin)</h3>
+                    <p>Autor: deepsuga</p>
+                    <p>Ano de Publicação: 16/08/2016</p>
+                    <button onclick="showSummary('summary3')">Mostrar Resumo</button>
+                    <p id="summary3" style="display: none;">Park Jimin, o maior traficante de Seul.Eu? Só uma garota como qualquer outraComo nos conhecemos? Em uma das piores noites da minha vida.Ele é possessivo, frio, egocêntrico e sarcástico,
+                     um completo psicopata.Por outro lado é sedutor e extremamente lindo.Se eu tenho medo dele? Não sei.Mas eu o desejo como nunca desejei alguém.
+                     -Você está mexendo com o perigo, ________ -ele disse e eu sorri fraco.-E você está mexendo comigo, Park Jimin.</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <script>
+        function showSummary(id) {
+            var summary = document.getElementById(id);
+            if (summary.style.display === "none") {
+                summary.style.display = "block";
+            } else {
+                summary.style.display = "none";
+            }
+        }
+    </script>
+
+</body>
+
+</html>
--- MK/Php/tags.php
+++ MK/Php/tags.php
@@ -1,53 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/sobre.css">
-    <title>Document</title>
-</head>
-<div class="background-container">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/tags.css">
+        <title>Document</title>
+    </head>
+    <div class="background-container">
 
-<body>
+    <body>
 
-<header>
-    <!-----------------LOGO----------------->
+    <header>
+        <!-----------------LOGO----------------->
 
-<img class="logo" src="../img/logoo.jpeg" width="80px">
+    <img class="logo" src="../img/logoo.jpeg" width="80px">
 
-<h1>Abc Fanfiction</h1>
-
-
-
-</header>
-
-<!---------------------------------MENU---------------------------------->
-<div class="barra">
+    <h1>Abc Fanfiction</h1>
 
 
-<nav class="link">
-<a href="index.php">PAGINA INICIAL</a>
-        <a href="../php/tags.php">TAGS</a>
-        <a href="../php/tags.php">NAVEGAR</a>
-        <a href="../php/tags.php">MINHAS HISTORIAS</a>
+
+    </header>
+
+    <!---------------------------------MENU---------------------------------->
+    <div class="barra">
+
+
+    <nav class="link">
+    <a href="index.php">PAGINA INICIAL</a>
+            <a href="../php/tags.php">TAGS</a>
+            <a href="../php/tags.php">NAVEGAR</a>
+            <a href="../php/tags.php">MINHAS HISTORIAS</a>
+            
+            <div id="divBusca">
+
+   
+    </div>
+
+    <div class="quadrado">
+    <div class="quadrado2">
+
+    <div class="quadrado3">
+    <h2>TAGS POPULARES</h2>
+
+    <section class="section">
+            <div class="topicsstart">
+                <h3>Encontre seus favoritos</h3>
+                <hr>
+            </div>
         
-        <div id="divBusca">
+            <div class="topics">
+                <a href="../php/romance.php">•  Romance</a>
+                <a href="../php/drama.php">•  Drama</a>
+                <a href="../php/naruto.php"">•  Naruto</a>
+                <a href="topic4.html">•  Comedia</a>
+                <a href="topic5.html">•  Amor</a>
+            </div>
+            
+            <div class="topics2">
+                <a href="topic8.html">•  Fantasia</a>
+                <a href="topic9.html">•  Tragedia</a>
+                <a href="topic10.html">•  Familia</a>
+                <a href="topic11.html">•  Poesia</a>
+                <a href="topic12.html">•  Ação</a>
+                <a href="topic13.html">•  Amizade</a>
+                <a href="topic13.html">•  Magia</a> 
+            </div>  
+                </section>
+        
 
-  <input type="text" id="txtBusca" placeholder="Pesquisar..."/>
-  <button id="btnBusca">Pesquisar</button>
-</div>
 
-<div class="quadrado">
-<div class="quadrado2">
-<div class="quadrado3">
-<h2> </h2>
-<div class="texto">
-   <p> A Abc Fanfiction é uma organização sem fins lucrativos, criada por fãs em 2007, para servir os interesses dos fãs, fornecendo acesso e preservando a história das obras de fãs e da cultura de fãs em suas inúmeras formas. Acreditamos que as obras de fãs são transformadoras e que as obras transformadoras são legítimas.
-Somos proativos e inovadores na proteção e defesa do nosso trabalho contra a exploração comercial e desafios legais. Preservamos nossa economia, valores e expressão criativa de fãs, protegendo e nutrindo nossos colegas fãs, nosso trabalho, nossos comentários, nossa história e nossa identidade, ao mesmo tempo em que fornecemos o acesso mais amplo possível à atividade de fãs para todos os fãs.
-A Abc oferece um local de hospedagem central não comercial e sem fins lucrativos para obras de fãs usando software de arquivamento de código aberto. </p>
-    
-
-
-</nav>
-</div>
+    </nav>
+    </div>
